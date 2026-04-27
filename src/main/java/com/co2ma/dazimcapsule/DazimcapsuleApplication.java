eda5d760ef68d2aae96c5537d594b05c13633bb0
Co2ma <window3357@gmail.com>
2026-04-28 00:54:13 +0900

    add: 처음 파일 업로드

---
+++ src/main/java/com/co2ma/dazimcapsule/DazimcapsuleApplication.java
@@ -0,0 +1,13 @@
+package com.co2ma.dazimcapsule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DazimcapsuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DazimcapsuleApplication.class, args);
+	}
+
+}
