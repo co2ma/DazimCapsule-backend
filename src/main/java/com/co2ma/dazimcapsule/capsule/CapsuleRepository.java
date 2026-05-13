2af8a31b8fd4a05fd8e5f41129908e413de1c8a1
박재영 <window3357@naver.com>
2026-05-13 14:55:45 +0900

    add: repo 클래스 추가

---
+++ src/main/java/com/co2ma/dazimcapsule/capsule/CapsuleRepository.java
@@ -0,0 +1,6 @@
+package com.co2ma.dazimcapsule.capsule;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CapsuleRepository extends JpaRepository<Capsule, Long> {
+}
