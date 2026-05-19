67c2b8d9f2ee3ce23b6bb8d77ada84b387718d9f
Co2ma <window3357@gmail.com>
2026-05-20 02:07:54 +0900

    add: flyway db V1__init 추가

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE capsule
+(
+    id              BIGINT AUTO_INCREMENT NOT NULL,
+    name            VARCHAR(255) NULL,
+    email           VARCHAR(255) NULL,
+    body            VARCHAR(255) NULL,
+    unique_link     VARCHAR(255) NULL,
+    expiration_date date NULL,
+    is_processed    BIT(1) NOT NULL,
+    CONSTRAINT pk_capsule PRIMARY KEY (id)
+);
