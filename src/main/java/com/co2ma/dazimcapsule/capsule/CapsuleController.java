9587d619511c1c835fafaefa12edd5cf21c2626a
Co2ma <window3357@gmail.com>
2026-05-21 01:40:56 +0900

    feat: 캡슐 전송 Post 명령 추가 및 Swagger 태그 추가

--- src/main/java/com/co2ma/dazimcapsule/capsule/CapsuleController.java
+++ src/main/java/com/co2ma/dazimcapsule/capsule/CapsuleController.java
@@ -1,12 +1,28 @@
 package com.co2ma.dazimcapsule.capsule;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller("/form")
+import java.time.LocalDate;
+
+@Tag(name="캡슐", description = "캡슐 전송 데이터")
+@RestController
+@RequestMapping("/form")
 public class CapsuleController {
-    private CapsuleDTO capsule;
 
+    private CapsuleService capsuleService;
+
+    @Operation(summary = "캡슐 전송하기", description = "캡슐을 DB로 전송 합니다.")
+    @PostMapping("/{link}")
+    public ResponseEntity<CapsuleRequestDTO> uploadCapsule(@PathVariable("link") String link,
+                                                            @RequestParam("date") LocalDate date,
+                                                            @RequestBody CapsuleRequestDTO capsuleRequestDTO) {
+
+        capsuleRequestDTO.setLink(link);
+        capsuleRequestDTO.setExpirationDate(date);
+        capsuleService.createCapsule(capsuleRequestDTO);
+        return ResponseEntity.ok(capsuleRequestDTO);
+    }
 }
