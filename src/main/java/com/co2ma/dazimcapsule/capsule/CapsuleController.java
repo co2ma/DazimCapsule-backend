943bc4030386b7ec2ffaa969d65e286247b6162c
Co2ma <window3357@gmail.com>
2026-05-21 02:08:55 +0900

    feat: capsuleService를 초기화 하기 위한 추가
    final class로 변경, @RequiredArgsConstructor 추가 이를 통해 자동으로 capsuleService를 초기화 할 수 있음.

--- src/main/java/com/co2ma/dazimcapsule/capsule/CapsuleController.java
+++ src/main/java/com/co2ma/dazimcapsule/capsule/CapsuleController.java
@@ -2,6 +2,7 @@ package com.co2ma.dazimcapsule.capsule;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,17 +11,17 @@ import java.time.LocalDate;
 @Tag(name="캡슐", description = "캡슐 전송 데이터")
 @RestController
 @RequestMapping("/form")
+@RequiredArgsConstructor
 public class CapsuleController {
 
-    private CapsuleService capsuleService;
+    private final CapsuleService capsuleService;
 
     @Operation(summary = "캡슐 전송하기", description = "캡슐을 DB로 전송 합니다.")
     @PostMapping("/{link}")
     public ResponseEntity<CapsuleRequestDTO> uploadCapsule(@PathVariable("link") String link,
                                                             @RequestParam("date") LocalDate date,
                                                             @RequestBody CapsuleRequestDTO capsuleRequestDTO) {
-
-        capsuleRequestDTO.setLink(link);
+        capsuleRequestDTO.setUniqueLink(link);
         capsuleRequestDTO.setExpirationDate(date);
         capsuleService.createCapsule(capsuleRequestDTO);
         return ResponseEntity.ok(capsuleRequestDTO);
